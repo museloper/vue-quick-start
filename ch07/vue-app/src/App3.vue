95ed355c493062844a1207222ac87a00ecfe1b02
museloper <nosf6842@naver.com>
2024-04-11 14:04:01 +0900

    자바스크립트 클래스 사용

---
+++ ch07/vue-app/src/App3.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>관심있는 K-POP 가수?</h2>
+    <hr />
+    <ul>
+      <CheckboxItem3 v-for="idol in idols" :key="idol.id" :idol="idol" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import CheckboxItem3 from './components/CheckboxItem3.vue'
+import Idol from './Idol'
+
+export default {
+  name: 'App',
+  components: {
+    CheckboxItem3,
+  },
+  data() {
+    return {
+      idols: [
+        new Idol(1, 'BTS', true),
+        new Idol(2, 'Black Pink', false),
+        new Idol(3, 'EXO', false),
+        new Idol(4, 'ITZY', false),
+      ],
+    }
+  },
+}
+</script>
--- ch07/vue-app/src/main.js
+++ ch07/vue-app/src/main.js
@@ -1,10 +1,13 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 // import App2 from './App2.vue'
-import CheckboxItem from './components/CheckboxItem.vue'
+import App3 from './App3.vue'
+// import CheckboxItem from './components/CheckboxItem.vue'
 // import CheckboxItem2 from './components/CheckboxItem2.vue'
+import CheckboxItem3 from './components/CheckboxItem3.vue'
 
-createApp(App).component('CheckboxItem', CheckboxItem).mount('#app')
+// createApp(App).component('CheckboxItem', CheckboxItem).mount('#app')
 // createApp(App2).component('CheckboxItem2', CheckboxItem2).mount('#app')
+createApp(App3).component('CheckboxItem3', CheckboxItem3).mount('#app')
